204f90eb86ad5d5e70daed54c2695e0b4907106c
Christian Winkler <chris@bashit.me>
2019-01-06 20:51:12 +0100

    fab button and fab dialog desing

--- Android App/app/src/main/java/me/projectx/needaticket/UserActivity.java
+++ Android App/app/src/main/java/me/projectx/needaticket/UserActivity.java
@@ -1,6 +1,11 @@
 package me.projectx.needaticket;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
@@ -8,7 +13,11 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.ViewAnimationUtils;
+import android.view.Window;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import me.projectx.needaticket.asynctask.TaskGetUser;
@@ -28,7 +37,7 @@ public class UserActivity extends AppCompatActivity implements InterfaceTaskDefa
     private TextView password;
     private TextView boughtTickets;
     private NavigationView navigation;
-    private FloatingActionButton fab_edit;
+    private FloatingActionButton fab_user;
 
     //super
     @Override
@@ -59,6 +68,7 @@ public class UserActivity extends AppCompatActivity implements InterfaceTaskDefa
         this.email = (TextView) findViewById(R.id.textview_email);
         this.password = (TextView) findViewById(R.id.textview_password);
         this.boughtTickets = (TextView) findViewById(R.id.textview_bought);
+        this.fab_user = (FloatingActionButton) findViewById(R.id.fab_user);
     }
 
     private void setListener(){
@@ -66,6 +76,80 @@ public class UserActivity extends AppCompatActivity implements InterfaceTaskDefa
         this.navigation.setItemIconTintList(null); //THIS LITTLE PIECE OF ... FIXES THE ICONS NOT SHOWING IN THE NAVMENU >:(
         this.setListenerNavigationHeader();
         this.swipeRefreshLayout.setOnRefreshListener(this);
+        this.fab_user.setOnClickListener(new editUserListener());
+    }
+
+    private class editUserListener implements View.OnClickListener{
+        @Override
+        public void onClick(View v) {
+           showDiag();
+        }
+    }
+
+    private void showDiag() {
+        final View dialogView = View.inflate(this,R.layout.dialog_edit_user,null);
+        final Dialog dialog = new Dialog(this,R.style.UserAlertStyle);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setContentView(dialogView);
+        ImageView imageView = (ImageView)dialog.findViewById(R.id.closeDialog);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                revealShow(dialogView, false, dialog);
+            }
+        });
+        dialog.setOnShowListener(new DialogInterface.OnShowListener() {
+            @Override
+            public void onShow(DialogInterface dialogInterface) {
+                revealShow(dialogView, true, null);
+            }
+        });
+        dialog.setOnKeyListener(new DialogInterface.OnKeyListener() {
+            @Override
+            public boolean onKey(DialogInterface dialogInterface, int i, KeyEvent keyEvent) {
+                if (i == KeyEvent.KEYCODE_BACK){
+
+                    revealShow(dialogView, false, dialog);
+                    return true;
+                }
+
+                return false;
+            }
+        });
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT));
+        dialog.show();
+    }
+
+    private void revealShow(View dialogView, boolean b, final Dialog dialog) {
+        final View view = dialogView.findViewById(R.id.dialog);
+        int w = view.getWidth();
+        int h = view.getHeight();
+        int endRadius = (int) Math.hypot(w, h);
+        int cx = (int) (fab_user.getX() + (fab_user.getWidth()/2));
+        int cy = (int) (fab_user.getY())+ fab_user.getHeight() + 56;
+        if(b){
+            Animator revealAnimator = ViewAnimationUtils.createCircularReveal(view, cx,cy, 0, endRadius);
+
+            view.setVisibility(View.VISIBLE);
+            revealAnimator.setDuration(700);
+            revealAnimator.start();
+        } else {
+            Animator anim =
+                    ViewAnimationUtils.createCircularReveal(view, cx, cy, endRadius, 0);
+
+            anim.addListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    super.onAnimationEnd(animation);
+                    dialog.dismiss();
+                    view.setVisibility(View.INVISIBLE);
+
+                }
+            });
+            anim.setDuration(700);
+            anim.start();
+        }
     }
 
     private void getUser(){

151c56ef3023a288c53d40a080e31c9124966a3a
Christian Winkler <chris@bashit.me>
2019-02-04 19:53:45 +0100

    not part of graphql

--- Android App/app/src/main/java/me/projectx/needaticket/interfaces/InterfaceTaskDefault.java
+++
@@ -1,5 +0,0 @@
-package me.projectx.needaticket.interfaces;
-public interface InterfaceTaskDefault {
-    void onPreExecute (Class resource);
-    void onPostExecute (Object result, Class resource);
-}

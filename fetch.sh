db40f63b5db3855deb9eb0bf5fca4f9fa7d8aabb
Christian Winkler <chris@bashit.me>
2019-02-04 20:09:16 +0100

    added a fetch script to fetch and pull all branches

---
+++ fetch.sh
@@ -0,0 +1,16 @@
+git fetch android-customer
+git pull
+git fetch android-qr
+git pull
+git fetch firebase
+git pull
+git fetch java-vendor
+git pull
+git fetch java-creator
+git pull
+git fetch planning
+git pull
+git fetch webservice-graphql
+git pull
+git fetch master
+git pull

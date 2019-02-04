d88c213708e896d73b2e3ad144a5e5cf348763c7
Christian Winkler <chris@bashit.me>
2019-02-04 20:12:28 +0100

    init

--- Android App/app/src/main/java/me/projectx/needaticket/interfaces/InterfaceGetTickets.java
+++
@@ -1,8 +0,0 @@
-package me.projectx.needaticket.interfaces;
-import java.util.ArrayList;
-
-import me.projectx.needaticket.pojo.Ticket;
-public interface InterfaceGetTickets {
-    void onPreExecute ();
-    void onPostExecute (ArrayList<Ticket> tickets);
-}
